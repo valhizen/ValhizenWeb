99be7b02951169b39764e62ac19c86514df35b4d
valhizen <valhize@gmail.com>
2025-10-12 13:04:42 +0545

    Change branch to master

---
+++ src/routes/+page.svelte
@@ -0,0 +1,34 @@
+
+<h1>
+At the foot of the mountain
+</h1>
+
+<h4>
+I have been stuck at the foot of the mountain for longer than I want to admit. I have circled it countless times, and in that I have gathered skills like supplies for the climb Hackathon wins, freelance work, C++ syntax I can write in my sleep. I am not at zero. But I am not climbing either.
+</h4>
+
+<h4>
+The summit is scary. I look up and see Linus Torvalds, Casey Muratori, Hidetaka Miyazaki and many more people who have built worlds, entire systems, and legacies. And when I look up, I am scared that I will never be capable of that and I will spend my entire life at the base.
+</h4>
+
+<h4>
+I want to build. Not for clout, not for recognition though I won't lie, I do want those things eventually. I want to build because there is a game in my head, an industry I want to build, something I want to make real. And I am starting with this alias "valhizen" so I can climb without the weight of my identity crushing me every single time I slip. Maybe one day I will give it a face, but right now I need to start moving. I want to do it publicly with accountability, or I will keep circling forever.
+
+</h4>
+
+<h4>
+Every time I start a project, the pattern is the same. I set up my dev environment, write some code, and when I hit a wall, I abandon it. I know what I need to do but have no idea how to actually do it. Recently I was building a start screen for a game. I designed a start screen in Krita, knew what I wanted, but when it came to implementation I froze. I didn't know where to start from. I closed the file. Eventually I changed my OS so I won't have to look at it again. And I realized what I needed wasn't motivation or inspiration. I needed accountability. I needed discipline. And maybe by making this public as "valhizen," it would force me to stop running.
+</h4>
+
+<h4>
+
+Now I'm committing. I'm going to build the start screen I abandoned the one I designed and then ran from. I'll design it again, and this time I'll code it. In one week, October 19th, 2025, I'll report back here with everything what I built, where I got stuck, how I pushed through.
+</h4>
+
+<h4>
+  This is the first real climb. Not another circle around the base. Not another dev environment that never becomes anything. valhizen starts now.
+</h4>
+
+
+
+
